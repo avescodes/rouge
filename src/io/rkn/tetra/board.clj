0bb9e8175d5b8dec0299861be32f26f86d638157
Ryan Neufeld <ryan@rkn.io>
2013-06-28 08:31:21 -0400

    Incrementally removing original game

--- src/io/rkn/tetra/board.clj
+++ src/io/rkn/tetra/board.clj
@@ -1,27 +1,6 @@
 (ns io.rkn.tetra.board
   (:require [io.rkn.tetra.pieces :as p]))
 
-;; Utility Functions
-(defn sizeb
-  "Game's Board's size as a row-major index (i.e. [1 2] for board [[x x]]).
-
-   Assumes uniform sized columns."
-  [game]
-  (let [board (:board game)]
-    [(count board) (count (first board))]))
-
-(defn mapb
-  "Map f over each element in board matrix."
-  [f board]
-  (mapv (fn [row] (mapv f row)) board))
-
-(defn set-in-board
-  "Set value at idx in game's :board to v. idx is in row-major order."
-   [game idx v]
-  (let [ks (concat [:board] idx)]
-    (assoc-in game ks v)))
-
-;; Game Logic predicates
 (defn inside-board?
   "Check if a row-major idx is not out-of-bounds of
    row-major board-size"
@@ -33,26 +12,6 @@
          (>= col 0)
          (< col cols))))
 
-(defn occupied?
-  "Check if row-major idx is non-empty (0) in board"
-  [board idx]
-  (not= 0 (get-in board idx)))
-
-(defn hit-bottom?
-  "Check if row-major idx is below bottom of board-size."
-  [board-size idx]
-  (let [[row _] idx
-        [rows _] board-size]
-    (>= row rows)))
-
-(defn collided?
-  "Check if a game-state contains any piece-to-landed or
-   piece-to-ground collisions."
-  [game]
-  (let [piece-idxs (p/occupied-idxs (:piece game))]
-    (or (some (partial occupied? (:board game)) piece-idxs)
-        (some (partial hit-bottom? (sizeb game)) piece-idxs))))
-
 (defn valid-posn?
   "Is :piece in a valid position (inside the board and in no occupied
    spaces)?"
@@ -61,17 +20,6 @@
     ;; Every part of the piece is inside the board
     (and (every? (partial inside-board? (sizeb game)) piece-idxs)
          (not-any? (partial occupied? (:board game)) piece-idxs))))
-
-(defn graft-piece-to-board
-  "Return the game where :piece has been shaded-in in :board."
-  [game]
-  (let [piece (:piece game)]
-    (loop [new-game game
-           [idx & remaining-idxs] (p/occupied-idxs piece)]
-      (if (nil? idx)
-        new-game
-        (recur (set-in-board new-game idx (:color piece))
-               remaining-idxs)))))
 
 (defn full-row?
   "Return true if a vector row contains no zero elements."
--- test/io/rkn/tetra/game_test.clj
+++ test/io/rkn/tetra/game_test.clj
@@ -2,16 +2,3 @@
   (:require [clojure.test :refer :all]
             [io.rkn.tetra.system :refer [new-game]]
             [io.rkn.tetra.game :refer :all]))
-
-(deftest level-test
-  (are [expected game]
-       (is (= expected (level game)))
-       1 {:lines-cleared 0}
-       1 {:lines-cleared 9}
-       2 {:lines-cleared 10}))
-
-(deftest bump-score-test
-  (are [original-game cleared expected]
-       (is (= expected (bump-score original-game cleared)))
-       {:score 0 :lines-cleared 0} 4 {:score 800 :lines-cleared 4}
-       {:score 1000 :lines-cleared 10} 2 {:score 1600 :lines-cleared 12}))
