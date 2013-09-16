8030aab57423b5f9a0090f74533f2a96be66edc4
Ryan Neufeld <ryan@rkn.io>
2013-09-16 11:07:51 -0400

    Regenerate project as 0.2.0 Pedestal app

--- project.clj
+++ project.clj
@@ -1,18 +1,24 @@
 (defproject io.rkn/rouge "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "That falling blocks game in Pedestal and core.async"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1586"]
+                 [org.clojure/clojurescript "0.0-1835"]
                  [domina "1.0.1"]
                  [ch.qos.logback/logback-classic "1.0.7" :exclusions [org.slf4j/slf4j-api]]
-                 [io.pedestal/pedestal.app "0.1.11-SNAPSHOT"]
-                 [io.pedestal/pedestal.app-tools "0.1.11-SNAPSHOT"]
-                 [com.cemerick/piggieback "0.0.4"]
+                 [io.pedestal/pedestal.app "0.2.1"]
+                 [io.pedestal/pedestal.app-tools "0.2.1"]
+                 [com.cemerick/piggieback "0.1.0"]
                  [io.rkn/core.async  "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:source-paths ["dev"]}}
   :min-lein-version "2.0.0"
   :source-paths ["app/src" "app/templates"]
   :resource-paths ["config"]
   :target-path "out/"
-  :aliases {"dumbrepl" ["trampoline" "run" "-m" "clojure.main/main"]}
-  :repl-options  {:nrepl-middleware  [cemerick.piggieback/wrap-cljs-repl]})
-
+  :repl-options  {:init-ns user
+                  :init (try
+                          (use 'io.pedestal.app-tools.dev)
+                          (catch Throwable t
+                            (println "ERROR: There was a problem loading io.pedestal.app-tools.dev")
+                            (clojure.stacktrace/print-stack-trace t)
+                            (println)))
+                  :welcome (println "Welcome to pedestal-app! Run (tools-help) to see a list of useful functions.")
+                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :main ^{:skip-aot true} io.pedestal.app-tools.dev)
